1fe9cc4c5b1815c7bb53f112acd12bc9ecdfedbc
tedspare <ted.spare@gmail.com>
2022-03-23 19:46:07 -0400

    Start about page. Adjust homepage.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,19 @@
 <script context="module">
-	export const prerender = true;
+  export const prerender = true;
 </script>
 
 <script>
-	import Counter from '$lib/Counter.svelte';
+  import Counter from "$lib/Counter.svelte";
 </script>
 
 <svelte:head>
-	<title>Home</title>
+  <title>Tailwindle</title>
 </svelte:head>
 
-<section class="flex flex-col justify-center items-center flex-1">
-	<h1 class="text-blue-400 font-extrabold w-full">
-		Welcome to Tailwindle
-	</h1>
-
-	<h2>
-		Try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+<section class="flex flex-col justify-center items-center">
+  <h1 class="">Welcome to Tailwindle</h1>
+  <h2>
+    Try editing <strong>src/routes/index.svelte</strong>
+  </h2>
+  <Counter />
 </section>
